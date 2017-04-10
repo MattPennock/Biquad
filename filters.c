5d2a9067ebc08e6a556fe54dd37ef2e6d0bd95a8
Matt P <mnp5131@psu.edu>
2017-04-10 15:36:56 -0400

    Laptop ded

--- filters.c
+++ filters.c
@@ -1,12 +1,13 @@
 #include <math.h>
-#include "biquad.c"
+#include <stdio.h>
+#include "biquad.h"
 
 const float pi = 3.141592654;
 
 /*
  *          Prototypes
  */
-void CONFIG_BandPass(biquad_t &bq, float center_freq, float bandwidth);
+void CONFIG_BandPass(biquad_t *bq, float center_freq, float bandwidth);
 
 /*
  *            Code
@@ -18,13 +19,13 @@ int main(void)
     CONFIG_BandPass(&bq, 0.5*pi, 0.1*pi);
 
     printf("Numerator Ring: \r\n");
-    RB3_print(bq.num_coeff);
+    RB3_print(&(bq.num_coeff));
 
     printf("Denominator Ring: \r\n");
-    RB3_print(bq.den_coeff);
+    RB3_print(&(bq.den_coeff));
 }
 
-void CONFIG_BandPass(biquad_t &bq, float center_freq, float bandwidth)
+void CONFIG_BandPass(biquad_t *bq, float center_freq, float bandwidth)
 {
     float beta = cosf(center_freq);
     float inv_beta = 1.0f / beta;
@@ -34,6 +35,8 @@ void CONFIG_BandPass(biquad_t &bq, float center_freq, float bandwidth)
     float a1 = beta * (1-alpha);
     float a2 = alpha;
     
-    BQ_setDen(&bq.den_coeff, a1, a2);
-    BQ_setNum(&bq.num_coeff, 1.0f, 0, 1.0f);
+    float num = (1.0f - alpha) / 2.0f;
+
+    BQ_setDen(bq, a1, a2);
+    BQ_setNum(bq, num, 0.0f, num);
 }
